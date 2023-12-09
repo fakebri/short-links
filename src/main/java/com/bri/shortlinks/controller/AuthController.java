2e6b7e8ce904faa232bee96a7d5722c2ca07d0b1
bri <77835042+fakebri@users.noreply.github.com>
2023-12-09 22:50:54 +0800

    使用session实现登录信息保存，实现登录状态接口，登出接口

--- src/main/java/com/bri/shortlinks/controller/AuthController.java
+++ src/main/java/com/bri/shortlinks/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @Api(tags = "身份验证接口")
 @CrossOrigin
@@ -19,17 +20,43 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody SlUser loginUser) {
-        SlUser resultUser = authService.queryUserByUserNameAndPassword(loginUser.getUserName(), loginUser.getPassword());
+    public ResponseEntity<?> login(HttpServletRequest request, @RequestBody SlUser loginUser) {
         ResultVo resultVo = new ResultVo();
+        if (request.getSession().getAttribute("userName") !=null ) {
+            resultVo.setMessage("用户已登录");
+            return ResponseEntity.ok(resultVo);
+        }
+        SlUser resultUser = authService.queryUserByUserNameAndPassword(loginUser.getUserName(), loginUser.getPassword());
         if (resultUser != null) {
+            request.getSession().setAttribute("userName", resultUser.getUserName());
             resultVo.setMessage("登录成功");
-            resultVo.setData(resultUser);
+            resultVo.setData(resultUser.getUserName());
             return ResponseEntity.ok(resultVo);
         } else {
             resultVo.setMessage("账户或密码错误");
             resultVo.setData(null);
             return ResponseEntity.badRequest().body(resultVo);
         }
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<?> getStatus(HttpServletRequest request) {
+        ResultVo resultVo = new ResultVo();
+        if (request.getSession().getAttribute("userName")!=null ) {
+            resultVo.setMessage("用户已登录");
+            resultVo.setData(request.getSession().getAttribute("userName"));
+            return ResponseEntity.ok(resultVo);
+        } else {
+            resultVo.setMessage("用户未登录");
+            return ResponseEntity.badRequest().body(resultVo);
+        }
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request) {
+        ResultVo resultVo = new ResultVo();
+        request.getSession().removeAttribute("userName");
+        resultVo.setMessage("用户已退出");
+        return ResponseEntity.ok(resultVo);
     }
 }
